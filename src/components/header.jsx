cb128a5f46472f98e5ee57279f89f968ee2f29e5
Popeydanielmate <14269@coderacademy.edu.au>
2024-11-09 13:22:58 +1100

    fix: img is correctly nested back in navbar, css still a WIP

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,17 +2,15 @@ import { NavLink } from "react-router-dom";
 import popeyHortLogo from "../assets/popey_hort_default.jpg";
 
 export default function Header() {
-	return (
-		<header>
-			<div id="headerBranding">
-				<img src={popeyHortLogo} alt="Popey's Horticulture Logo" />
-			</div>
-			<nav id="headerNavbar">
-				<NavLink to={"/"}>Home</NavLink>
-				<NavLink to={"/"}>About</NavLink>
-				<NavLink to={"/"}>Services</NavLink>
-				<NavLink to={"/"}>Gallery</NavLink>
-			</nav>
-		</header>
-	);
+  return (
+    <header>
+      <nav id="headerNavbar">
+        <img src={popeyHortLogo} alt="Popey's Horticulture Logo" />
+        <NavLink to={"/"}>Home</NavLink>
+        <NavLink to={"/"}>About</NavLink>
+        <NavLink to={"/"}>Services</NavLink>
+        <NavLink to={"/"}>Gallery</NavLink>
+      </nav>
+    </header>
+  );
 }
