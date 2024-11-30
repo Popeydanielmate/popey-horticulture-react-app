658f0ac64dc19a05105bee6a2a87dc026847d138
Popeydanielmate <14269@coderacademy.edu.au>
2024-11-30 14:20:35 +1100

    feat: footer links and icons updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,35 @@
-export default function Footer(){
-	return (
-		<footer>
-			<h6>Copyright 2024 Popey's Horticulture. Site made by Daniel</h6>
-			<a href="mailto:ben@popeyshorticulture.com">Email us!</a>
-		</footer>
-	)
+import envelopeIcon from "../assets/envelope_icon.png";
+import facebookIcon from "../assets/facebook-icon.png";
+import instaIcon from "../assets/insta-icon.png";
+
+export default function Footer() {
+  return (
+    <footer>
+      <h6>Copyright 2024 Popey's Horticulture. Site made by Daniel</h6>
+      <div className="footer-links">
+        <a href="mailto:ben@popeyshorticulture.com" className="footer-link">
+          <img src={envelopeIcon} alt="Email us" />
+          <span>Email</span>
+        </a>
+        <a
+          href="https://www.facebook.com/popeyhorticulture"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-link"
+        >
+          <img src={facebookIcon} alt="Visit our Facebook" />
+          <span>Facebook</span>
+        </a>
+        <a
+          href="https://www.instagram.com/popeys.horticulture/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-link"
+        >
+          <img src={instaIcon} alt="Visit our Instagram" />
+          <span>Instagram</span>
+        </a>
+      </div>
+    </footer>
+  );
 }
