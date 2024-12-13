a50034b59cc634fcd44722bd51a73f060314174e
Popeydanielmate <14269@coderacademy.edu.au>
2024-12-13 15:38:05 +1100

    fix: icon links html updated with better names but css still needs adjusting

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -9,30 +9,31 @@ export default function Homepage() {
         <section id="home">
           <h1>Popey's Horticulture - Lawns & Gardens</h1>
           <p>Horticulturist based in Leeton, servicing the MIA region specialising in lawn maintenance & hedging</p>
-          <div className="footer-links">
-        <a href="mailto:ben@popeyshorticulture.com" className="footer-link">
-          <img src={envelopeIcon} alt="Email us" />
-          <span>Email</span>
-        </a>
-        <a
-          href="https://www.facebook.com/popeyhorticulture"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="footer-link"
-        >
-          <img src={facebookIcon} alt="Visit our Facebook" />
-          <span>Facebook</span>
-        </a>
-        <a
-          href="https://www.instagram.com/popeys.horticulture/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="footer-link"
-        >
-          <img src={instaIcon} alt="Visit our Instagram" />
-          <span>Instagram</span>
-        </a>
-      </div>
+            <div className="homepage-links">
+                <a href="mailto:ben@popeyshorticulture.com" className="homepage-link">
+                    <img src={envelopeIcon} alt="Email us" />
+                    <span>Email</span>
+                </a>
+                <a
+                    href="https://www.facebook.com/popeyhorticulture"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="homepage-link"
+                >
+                    <img src={facebookIcon} alt="Visit our Facebook" />
+                    <span>Facebook</span>
+                </a>
+                <a
+                    href="https://www.instagram.com/popeys.horticulture/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="homepage-link"
+                >
+                    <img src={instaIcon} alt="Visit our Instagram" />
+                    <span>Instagram</span>
+                </a>
+                </div>
+
           <img 
             src={hort_cover_photo} 
             alt="Beautiful garden and lawn maintained by Popey's Horticulture" 
