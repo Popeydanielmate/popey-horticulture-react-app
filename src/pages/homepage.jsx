745fee33da1554151a341ecc0e9b9993bcae973b
Popeydanielmate <14269@coderacademy.edu.au>
2024-12-13 15:16:55 +1100

    feat: contact link icons on body of homepage now but their css is still a WIP

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,4 +1,7 @@
 import hort_cover_photo from "../assets/hort_cover_photo.jpg"
+import envelopeIcon from "../assets/envelope_icon.png";
+import facebookIcon from "../assets/facebook-icon.png";
+import instaIcon from "../assets/insta-icon.png";
 
 export default function Homepage() {
     return (
@@ -6,6 +9,30 @@ export default function Homepage() {
         <section id="home">
           <h1>Popey's Horticulture - Lawns & Gardens</h1>
           <p>Horticulturist based in Leeton, servicing the MIA region specialising in lawn maintenance & hedging</p>
+          <div className="footer-links">
+        <a href="mailto:ben@popeyshorticulture.com" className="footer-link">
+          <img src={envelopeIcon} alt="Email us" />
+          <span>Email</span>
+        </a>
+        <a
+          href="https://www.facebook.com/popeyhorticulture"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-link"
+        >
+          <img src={facebookIcon} alt="Visit our Facebook" />
+          <span>Facebook</span>
+        </a>
+        <a
+          href="https://www.instagram.com/popeys.horticulture/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-link"
+        >
+          <img src={instaIcon} alt="Visit our Instagram" />
+          <span>Instagram</span>
+        </a>
+      </div>
           <img 
             src={hort_cover_photo} 
             alt="Beautiful garden and lawn maintained by Popey's Horticulture" 
