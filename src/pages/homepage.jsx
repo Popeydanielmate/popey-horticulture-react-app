ef01bb19628d0b6c295d8ea3c33821f85c112b84
Popeydanielmate <14269@coderacademy.edu.au>
2024-12-15 13:47:52 +1100

    feat: photo slideshow implemented on homepage but nav buttons in css still need some work

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,58 +1,104 @@
-import hort_cover_photo from "../assets/hort_cover_photo.jpg"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import hort_cover_photo1 from "../assets/hort_cover_photo1.jpg";
+import hort_cover_photo2 from "../assets/hort_cover_photo2.jpg";
+import hort_cover_photo3 from "../assets/hort_cover_photo3.jpg";
+import hort_cover_photo4 from "../assets/hort_cover_photo4.jpg";
+import hort_cover_photo5 from "../assets/hort_cover_photo5.jpg";
+import hort_cover_photo6 from "../assets/hort_cover_photo6.jpg";
+import hort_cover_photo7 from "../assets/hort_cover_photo7.jpg";
+import hort_cover_photo from "../assets/hort_cover_photo.jpg";
 import envelopeIcon from "../assets/envelope_icon.png";
 import facebookIcon from "../assets/facebook-icon.png";
 import instaIcon from "../assets/insta-icon.png";
 
 export default function Homepage() {
+    const settings = {
+        dots: true,          // Show navigation dots
+        infinite: true,      // Loop through images
+        speed: 500,          // Transition speed
+        slidesToShow: 1,     // One image at a time
+        slidesToScroll: 1,   // Scroll one image at a time
+        autoplay: true,      // Automatic sliding
+        autoplaySpeed: 3000, // Time between slides (in ms)
+    };
+
     return (
       <div>
         <section id="home">
           <h1>Popey's Horticulture - Lawns & Gardens</h1>
           <p>Horticulturist based in Leeton, servicing the MIA region specialising in lawn maintenance & hedging</p>
-            <div className="homepage-links">
-                <a href="mailto:ben@popeyshorticulture.com" className="homepage-link">
-                    <img src={envelopeIcon} alt="Email us" />
-                    <span>Email</span>
-                </a>
-                <a
-                    href="https://www.facebook.com/popeyhorticulture"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="homepage-link"
-                >
-                    <img src={facebookIcon} alt="Visit our Facebook" />
-                    <span>Facebook</span>
-                </a>
-                <a
-                    href="https://www.instagram.com/popeys.horticulture/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="homepage-link"
-                >
-                    <img src={instaIcon} alt="Visit our Instagram" />
-                    <span>Instagram</span>
-                </a>
-                </div>
+          
+          {/* Social Media Links */}
+          <div className="homepage-links">
+              <a href="mailto:ben@popeyshorticulture.com" className="homepage-link">
+                  <img src={envelopeIcon} alt="Email us" />
+                  <span>Email</span>
+              </a>
+              <a
+                  href="https://www.facebook.com/popeyhorticulture"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="homepage-link"
+              >
+                  <img src={facebookIcon} alt="Visit our Facebook" />
+                  <span>Facebook</span>
+              </a>
+              <a
+                  href="https://www.instagram.com/popeys.horticulture/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="homepage-link"
+              >
+                  <img src={instaIcon} alt="Visit our Instagram" />
+                  <span>Instagram</span>
+              </a>
+          </div>
 
-          <img 
-            src={hort_cover_photo} 
-            alt="Beautiful garden and lawn maintained by Popey's Horticulture" 
-          />
+          {/* Image Slideshow */}
+          <Slider {...settings}>
+              <div>
+                  <img src={hort_cover_photo} alt="Main Cover Photo" />
+              </div>
+              <div>
+                  <img src={hort_cover_photo1} alt="Garden Image 1" />
+              </div>
+              <div>
+                  <img src={hort_cover_photo2} alt="Garden Image 2" />
+              </div>
+              <div>
+                  <img src={hort_cover_photo3} alt="Garden Image 3" />
+              </div>
+              <div>
+                  <img src={hort_cover_photo4} alt="Garden Image 4" />
+              </div>
+              <div>
+                  <img src={hort_cover_photo5} alt="Garden Image 5" />
+              </div>
+              <div>
+                  <img src={hort_cover_photo6} alt="Garden Image 6" />
+              </div>
+              <div>
+                  <img src={hort_cover_photo7} alt="Garden Image 7" />
+              </div>
+          </Slider>
         </section>
+        
         <section id="about">
           <h2>About Us</h2>
           <p>Details about the business...</p>
         </section>
+        
         <section id="services">
           <h2>Our Services</h2>
           <p>Description of services offered...</p>
         </section>
+        
         <section id="gallery">
           <h2>Gallery</h2>
           <p>Some images of our work...</p>
         </section>
       </div>
     );
-  }
-  
-  
+}
